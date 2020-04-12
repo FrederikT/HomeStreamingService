ab6016626d5ade1949c7a55a1829111c6f4acc32
FrederikT <frederikthuss@outlook.de>
2020-04-12 14:19:40 +0200

    extracted printCards to htmlElements.inc.php
    Frontpage now filled with content from database

--- includes/htmlElements.inc.php
+++ includes/htmlElements.inc.php
@@ -389,7 +389,171 @@ class HtmlElements
 
 
 
+    static function printPaginator(){
+        print ('<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+												<div class="pager_wrapper gc_blog_pagination">
+													<ul class="pagination">
+														<li><a href="#"><i class="flaticon-left-arrow"></i></a>
+														</li>
+														<li><a href="#">1</a>
+														</li>
+														<li><a href="#">2</a>
+														</li>
+														<li class="prs_third_page"><a href="#">3</a>
+														</li>
+														<li class="hidden-xs"><a href="#">4</a>
+														</li>
+														<li><a href="#"><i class="flaticon-right-arrow"></i></a>
+														</li>
+													</ul>
+												</div>
+											</div>');
+    }
 
+    static function printCard($type, $title, $id){
+        print ('<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 prs_upcom_slide_first">
+                                                            <div class="prs_upcom_movie_box_wrapper prs_mcc_movie_box_wrapper">
+                                                                <div class="prs_upcom_movie_img_box">
+                                                                    <img src="images/content/movie_category/up1.jpg" alt="movie_img" />
+                                                                    <div class="prs_upcom_movie_img_overlay"></div>
+                                                                    <div class="prs_upcom_movie_img_btn_wrapper">
+                                                                        <ul>
+                                                                            <li><a href="#">View Trailer</a>
+                                                                            </li>
+                                                                            <li><a href="#">View Details</a>
+                                                                            </li>
+                                                                        </ul>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="prs_upcom_movie_content_box">
+                                                                    <div class="prs_upcom_movie_content_box_inner">
+                                                                        <h2><a href="');
+        if(strcasecmp($type, "Adaptation") == 0){
+            print "show.html?show=";
+            print $title;
+            print "&id=";
+            print $id;
+        }else if(strcasecmp($type, "Movie") == 0 || strcasecmp($type, "Episode") == 0){
+            print "streaming.html?".$type."=";
+            print $title;
+            print "&id=";
+            print $id;
+        }
+        print ('">');
+        print $title; print ('</a></h2>
+                                                                        <p>Drama , Acation</p>
+                                                                        <!--
+                                                                        <i class="fa fa-star"></i>
+                                                                        <i class="fa fa-star"></i>
+                                                                        <i class="fa fa-star"></i>
+                                                                        <i class="fa fa-star-o"></i>
+                                                                        <i class="fa fa-star-o"></i>-->
+                                                                    </div>
+                                                                    <div class="prs_upcom_movie_content_box_inner_icon">
+                                                                        <ul>
+                                                                            <!--<li><a href="movie_booking.html"><i class="flaticon-cart-of-ecommerce"></i></a>
+                                                                            </li>-->
+                                                                        </ul>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                ');
+    }
+
+    static function printSmallCard($type, $title, $id){
+        print ('<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                                                            <div class="prs_upcom_movie_box_wrapper prs_mcc_movie_box_wrapper">
+                                                                <div class="prs_upcom_movie_img_box">
+                                                                    <img src="images/content/movie_category/up5.jpg" alt="movie_img" />
+                                                                    <div class="prs_upcom_movie_img_overlay"></div>
+                                                                    <div class="prs_upcom_movie_img_btn_wrapper">
+                                                                        <ul>
+                                                                            <li><a href="#">View Trailer</a>
+                                                                            </li>
+                                                                            <li><a href="#">View Details</a>
+                                                                            </li>
+                                                                        </ul>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="prs_upcom_movie_content_box">
+                                                                    <div class="prs_upcom_movie_content_box_inner">
+                                                                        <h2><a href="');
+        if(strcasecmp($type, "Adaptation") == 0){
+            print "show.html?show=";
+            print $title;
+            print "&id=";
+            print $id;
+        }else if(strcasecmp($type, "Movie") == 0 || strcasecmp($type, "Episode") == 0){
+            print "streaming.html?".$type."=";
+            print $title;
+            print "&id=";
+            print $id;
+        }
+        print ('">');
+        print $title; print ('</a></h2>
+                                                                        <p>Drama , Acation</p>
+                                                                        <!--
+                                                                        <i class="fa fa-star"></i>
+                                                                        <i class="fa fa-star"></i>
+                                                                        <i class="fa fa-star"></i>
+                                                                        <i class="fa fa-star-o"></i>
+                                                                        <i class="fa fa-star-o"></i>-->
+                                                                    </div>
+                                                                    <div class="prs_upcom_movie_content_box_inner_icon">
+                                                                        <ul>
+                                                                            <!--<li><a href="movie_booking.html"><i class="flaticon-cart-of-ecommerce"></i></a>
+                                                                            </li>-->
+                                                                        </ul>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                ');
+    }
+
+    static function printListCard($type, $title, $id){
+        print('<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                                            <div class="prs_mcc_list_movie_main_wrapper">
+                                                                <div class="prs_mcc_list_movie_img_wrapper">
+                                                                    <img src="images/content/movie_category/up1.jpg" alt="categoty_img">
+                                                                </div>
+                                                                <div class="prs_mcc_list_movie_img_cont_wrapper">
+                                                                    <div class="prs_mcc_list_left_cont_wrapper">
+                                                                        <h2><a href="');
+        if(strcasecmp($type, "Adaptation") == 0){
+            print "show.html?show=";
+            print $title;
+            print "&id=";
+            print $id;
+        }else if(strcasecmp($type, "Movie") == 0 || strcasecmp($type, "Episode") == 0){
+            print "streaming.html?".$type."=";
+            print $title;
+            print "&id=";
+            print $id;
+        }
+        print ('">');
+        print $title; print ('</a></h2>
+                                                                        <p>Drama , Acation &nbsp;&nbsp;&nbsp;<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i>
+                                                                        </p>
+                                                                        <h4>Movie Director - Jhon Doe</h4>
+                                                                    </div>
+                                                                    <div class="prs_mcc_list_right_cont_wrapper">	<a href="#"><i class="flaticon-cart-of-ecommerce"></i></a>
+                                                                    </div>
+                                                                    <div class="prs_mcc_list_bottom_cont_wrapper">
+                                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis trud exercitation ullamco.</p>
+                                                                        <ul>
+                                                                            <li><a href="#">View Trailer</a>
+                                                                            </li>
+                                                                            <li><a href="#">View Details</a>
+                                                                            </li>
+                                                                        </ul>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+											     ');
+    }
 
 
 
